fe9f831acf72ca4c5f3104bdd215881d08b21652
miguelestradam36 <miguelestradam36@gmail.com>
2025-05-29 13:24:59 -0600

    Select all data from tables

---
+++ select.sql
@@ -0,0 +1,20 @@
+/*Select all rows from the clients table*/
+SELECT * FROM Clients;
+
+/*Select all rows from the invoices table*/
+SELECT * FROM Invoices;
+
+/*Select all rows from the Invoices Lines table*/
+SELECT * FROM InvoiceLines;
+
+/*Select all rows from the stock table*/
+SELECT * FROM Stock;
+
+/*Select all rows from the invoices table*/
+SELECT * FROM Colors;
+
+/*Select all rows from the Invoices Lines table*/
+SELECT * FROM Countries;
+
+/*Select all rows from the stock table*/
+SELECT * FROM Colors;
